81ec0c9c6109b0d0a329eec2bde2b28a569648e3
Vedansh Agrawal <agrawal.vedansh@gmail.com>
2024-12-25 16:25:48 +0530

    Added ability to assign task to a user

--- app/javascript/src/components/Tasks/Show.jsx
+++ app/javascript/src/components/Tasks/Show.jsx
@@ -7,6 +7,7 @@ import { Button, Container, PageLoader } from "components/commons";
 
 const Show = () => {
   const [task, setTask] = useState([]);
+  const [assignedUser, setAssignedUser] = useState([]);
   const [pageLoading, setPageLoading] = useState(true);
   const { slug } = useParams();
   const history = useHistory();
@@ -18,9 +19,10 @@ const Show = () => {
   const fetchTaskDetails = async () => {
     try {
       const {
-        data: { task },
+        data: { task, assigned_user },
       } = await tasksApi.show(slug);
       setTask(task);
+      setAssignedUser(assigned_user);
       setPageLoading(false);
     } catch (error) {
       logger.error(error);
@@ -42,16 +44,20 @@ const Show = () => {
         <div className="mt-8 flex w-full items-start justify-between gap-x-6">
           <div className="flex flex-col gap-y-2">
             <h2 className="text-3xl font-semibold">{task?.title}</h2>
+            <div className="flex items-center gap-x-6">
+              <p className="text-base text-gray-700">
+                <span className="font-semibold">Assigned to: </span>
+                {assignedUser?.name}
+              </p>
+            </div>
           </div>
-          <div className="flex items-center justify-end gap-x-3">
-            <Button
-              buttonText="Edit"
-              icon="edit-line"
-              size="small"
-              style="secondary"
-              onClick={updateTask}
-            />
-          </div>
+          <Button
+            buttonText="Edit"
+            icon="edit-line"
+            size="small"
+            style="secondary"
+            onClick={updateTask}
+          />
         </div>
       </div>
     </Container>
